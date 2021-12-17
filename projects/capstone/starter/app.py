58fc39d9f50d0d8f5b768252d8da4da7a27cccc1
leonchan93 <leonc3490@gmail.com>
2021-12-17 11:42:32 +0000

    changes 2.0

--- projects/capstone/starter/app.py
+++ projects/capstone/starter/app.py
@@ -1,15 +1,289 @@
-from flask import Flask
+from flask import Flask, request, abort, jsonify
 from flask_cors import CORS
 from models import *
 
+def paginate_actors(request, selection):
+    page = request.args.get('page', 1, type=int)
+    start = (page - 1) * ACTORS_PER_PAGE
+    end = start + ACTORS_PER_PAGE
 
-def create_app():
+    actors = [actor.format() for actor in selection]
+    current_actors = actors[start:end]
+
+    return current_actors
+
+ACTORS_PER_PAGE = 10
+
+def paginate_movies(request, selection):
+    page = request.args.get('page', 1, type=int)
+    start = (page - 1) * MOVIES_PER_PAGE
+    end = start + MOVIES_PER_PAGE
+
+    questions = [question.format() for question in selection]
+    current_movies = questions[start:end]
+
+    return current_movies
+
+MOVIES_PER_PAGE = 10
+
+def create_app(test_config=None):
+    # create and configure the app
     app = Flask(__name__)
-    @app.route('/')
-    def hello_world():
-        return 'Hello, World!'
-    return app
-app = create_app()
+    setup_db(app)
+    cors = CORS(app)
 
-db = SQLAlchemy(app)
-migrate = Migrate(app, db)
+    '''
+  @TODO: Set up CORS. Allow '*' for origins.
+  '''
+
+    '''
+  @TODO: Use the after_request decorator to set Access-Control-Allow
+  '''
+    @app.after_request
+    def after_request(response):
+        response.headers.add('Access-Control-Allow-Headers',
+                             'Content-Type, Authorization, true')
+        response.headers.add('Access-Control-Allow-Methods',
+                             'GET, PUT, POST, PATCH, DELETE, OPTIONS')
+        return response
+    '''
+  @TODO:
+  Create an endpoint to handle GET requests
+  for all available categories.
+  '''
+    @app.route('/actors')
+    def get_actors():
+        selection = Actors.query.order_by(Actors.id).all()
+        current_actors = paginate_actors(request, selection)
+
+        if len(current_actors) == 0:
+            abort(404)
+
+        return jsonify({
+         'success': True,
+         'actors': current_actors,
+         'total_actors': len(Actors.query.all())
+        })
+
+
+    @app.route('/movies')
+    def get_movies():
+        selection = Movies.query.order_by(Movies.id).all()
+        current_movies = paginate_movies(request, selection)
+
+        if len(current_movies) == 0:
+            abort(404)
+
+        return jsonify({
+         'success': True,
+         'movies': current_movies,
+         'total_movies': len(Movies.query.all())
+
+        })
+
+    @app.route('/actors/<int:actor_id>', methods=['DELETE'])
+    def delete_actor(actor_id):
+
+        selection = Actors.query.order_by(Actors.id).all()
+        current_actors = paginate_actors(request, selection)
+
+        try:
+            actor = Actors.query.filter(
+                Actors.id == actor_id).one_or_none()
+
+            if actor is None:
+                abort(404)
+
+            actor.delete()
+
+            return jsonify({
+                'success': True,
+                'deleted': actor_id,
+                'actors': current_actors,
+                'total_actors': len(Actors.query.all())
+                })
+
+        except:
+                abort(422)
+
+    @app.route('/movies/<int:movie_id>', methods=['DELETE'])
+    def delete_movie(movie_id):
+
+        selection = Movies.query.order_by(Movies.id).all()
+        current_movies = paginate_movies(request, selection)
+
+        try:
+            actor = Movies.query.filter(
+                Movies.id == movie_id).one_or_none()
+
+            if actor is None:
+                abort(404)
+
+            actor.delete()
+
+            return jsonify({
+                'success': True,
+                'deleted': movie_id,
+                'actors': current_movies,
+                'total_actors': len(Movies.query.all())
+                })
+
+        except:
+                abort(422)
+
+    @app.route('/add-actor', methods=['POST'])
+    def create_actor():
+        body = request.get_json()
+
+        new_actor = body.get('actor', None)
+
+        try:
+          actor = Actors(actor=new_actor)
+          actor.insert()
+
+          selection = Actors.query.all()
+          current_actors = paginate_actors(request, selection)
+
+          return jsonify({
+              'success': True,
+              'created': actor.id,
+              'actors': current_actors,
+              'total_actors': len(Actors.query.all())
+            })
+
+        except:
+            abort(422)
+
+    @app.route('/add-movie', methods=['POST'])
+    def create_movie():
+        body = request.get_json()
+
+        new_movie = body.get('movie', None)
+
+        try:
+          movie = Movies(movie=new_movie)
+          movie.insert()
+
+          selection = Movies.query.all()
+          current_movies = paginate_movies(request, selection)
+
+          return jsonify({
+              'success': True,
+              'created': movie.id,
+              'actors': current_movies,
+              'total_actors': len(Movies.query.all())
+            })
+
+        except:
+            abort(422)
+
+    @app.route('/actors/<int:actor_id>', methods=['PATCH'])
+    def patch_actors(payload, actor_id):
+
+        body = request.get_json()
+
+        new_name = body.get('name', None)
+        new_age = body.get('age', None)
+        new_gender = body.get('gender', None)
+
+        if new_name is None and new_age is None and new_gender is None:
+            abort(422)
+
+        try:
+            actor = Actors.query.filter(
+                Actors.id == actor_id).one_or_none()
+
+            if actor is None:
+                abort(404)    
+
+            actor.update()
+
+            selection = Actors.query.order_by(Actors.id).all()
+            current_actors = paginate_actors(request, selection)
+
+            return jsonify({
+                'success': True,
+                'actors': current_actors,
+                'total_actors': len(Actors.query.all())
+            })
+
+        except:
+            abort(422)
+
+    @app.route('/movies/<int:movie_id>', methods=['PATCH'])
+    def patch_movies(payload, movie_id):
+
+        body = request.get_json()
+
+        new_title = body.get('title', None)
+        new_releaseDate = body.get('releaseDate', None)
+
+        if new_title is None and new_releaseDate is None:
+            abort(422)
+
+        try:
+            movie = Movies.query.filter(
+                Movies.id == movie_id).one_or_none()
+
+            if movie is None:
+                abort(404)  
+
+            movie.update()
+
+            selection = Movies.query.order_by(Movies.id).all()
+            current_movies = paginate_movies(request, selection)
+
+            return jsonify({
+                'success': True,
+                'movies': current_movies,
+                'total_movies': len(Movies.query.all())
+            })
+
+        except:
+            abort(422)
+            
+    '''
+  @TODO:
+  Create error handlers for all expected errors
+  including 404 and 422.
+    '''
+
+    @app.errorhandler(404)
+    def not_found(error):
+        return jsonify({
+            "success": False,
+            "error": 404,
+            "message": "resource not found"
+          }), 404
+
+    @app.errorhandler(422)
+    def unprocessable(error):
+        return jsonify({
+            "success": False,
+            "error": 422,
+            "message": "unprocessable"
+          }), 422
+
+    @app.errorhandler(400)
+    def bad_request(error):
+        return jsonify({
+            "success": False,
+            "error": 400,
+            "message": "bad request"
+          }), 400
+
+    @app.errorhandler(405)
+    def method_not_allowed(error):
+        return jsonify({
+            "success": False,
+            "error": 405,
+            "message": "method not allowed"
+          }), 405
+
+    @app.errorhandler(500)
+    def server_error(error):
+        return jsonify({
+            "error": 500,
+            "message": "server error"
+          }), 500
+    return app
--- projects/capstone/starter/models.py
+++ projects/capstone/starter/models.py
@@ -3,37 +3,58 @@ from flask_sqlalchemy import SQLAlchemy
 from flask import Flask
 from sqlalchemy import Column, DateTime, Integer, String
 from flask_migrate import Migrate
-#----------------------------------------------------------------------------#
-# App Config.
-#----------------------------------------------------------------------------#
 
-DB_HOST = os.getenv('DB_HOST', '127.0.0.1:5432')  
+app = Flask(__name__)
+app.config.from_object('config')
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+DB_HOST = os.getenv('DB_HOST', '127.0.0.1:5432')  #May have to delete or rework this part of the code if migrate doesn't work with this
 DB_USER = os.getenv('DB_USER', 'postgres')  
 DB_PASSWORD = os.getenv('DB_PASSWORD', 'Projecta2008')  
 DB_NAME = os.getenv('DB_NAME', 'castingagency')  
 DB_PATH = 'postgresql+psycopg2://{}:{}@{}/{}'.format(DB_USER, DB_PASSWORD, DB_HOST, DB_NAME)
 db = SQLAlchemy()
 
-'''
-setup_db(app)
-    binds a flask application and a SQLAlchemy service
-'''
 def setup_db(app, database_path=DB_PATH):
     app.config["SQLALCHEMY_DATABASE_URI"] = database_path
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
     db.app = app
     db.init_app(app)
+    db.create_all()
+
+def db_create_all():
+    db.create_all()
+
+def db_drop_and_create_all():
+    db.drop_all()
+    db.create_all()
+
+def create_dummy_data():
+    # Get some dummy data into the db
+    actor1 = Actors(name='1', age=20, gender='Male')
+    actor1.insert()
+    actor2 = Actors(name='2', age=30, gender='Female')
+    actor2.insert()
+    actor3 = Actors(name='3', age=40, gender='Female')
+    actor3.insert()
+    movie1 = Movies(title='Movie1', releaseDate='2021-12-01')
+    movie1.insert()
+    movie2 = Movies(title='Movie2', releaseDate='2021-12-021')
+    movie2.insert()
+    movie3 = Movies(title='Movie3', releaseDate='2021-12-03')
+    movie3.insert()
 
 class Actors(db.Model):
     __tablename__ = 'Actors'
 
     id = db.Column(db.Integer, primary_key=True)
     name  = db.Column(db.String)
-    age = db.Column(db.String(120))
+    age = db.Column(db.Integer)
     gender = db.Column(db.String(120))
     
     def __repr__(self):
-        return f'<actor id: {self.id}, name: {self.name}, age: {self.age}, gender: {self.gender}>'
+        return f'<Actors id: {self.id}, name: {self.name}, age: {self.age}, gender: {self.gender}>'
 
     def __init__(self, name, age, gender):
         self.name = name
@@ -64,15 +85,14 @@ class Movies(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     title  = db.Column(db.String)
-    releaseDate = db.Column(db.String(120))
+    releaseDate = db.Column(db.DateTime, nullable=False)
 
     def __repr__(self):
-         return f'<movie id: {self.id}, title: {self.title}, releaseDate: {self.releaseDate}>'
+         return f'<Movies id: {self.id}, title: {self.title}, releaseDate: {self.releaseDate}>'
     
-
-    def __init__(self, title, release_date):
+    def __init__(self, title, releaseDate):
         self.title = title
-        self.release_date = release_date
+        self.releaseDate = releaseDate
 
     def insert(self):
         db.session.add(self)
@@ -89,5 +109,5 @@ class Movies(db.Model):
         return {
             'id': self.id,
             'title': self.title,
-            'release_date': self.release_date
+            'release_date': self.releaseDate
         }
