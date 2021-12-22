e33a2f2599fb4d427d7c54a15d8d3f0fd988e546
Leon Chan <leonc3490@gmail.com>
2021-12-22 10:13:09 +0000

    Test

--- projects/capstone/starter/test_app.py
+++ projects/capstone/starter/test_app.py
@@ -8,7 +8,7 @@ from app import create_app
 from models import *
 
 class TriviaTestCase(unittest.TestCase):
-    """This class represents the trivia test case"""
+    """This class represents the trivia test case TEST"""
 
     def setUp(self):
         """Define test variables and initialize app."""
