f6756cff9c3f3cb3ea3cbd92411cbe6fed635050
leonchan93 <leonc3490@gmail.com>
2021-12-24 23:54:43 +0000

    Add Auth0 configurations

--- projects/capstone/starter/setup.sh
+++ projects/capstone/starter/setup.sh
@@ -1,1 +1,5 @@
 export DATABASE_URL = 'postgresql://postgres:Projecta2008@localhost:5432/castingagency'
+
+export Auth0_Domain_Name='dev-fwwfme-j.us.auth0.com'
+export JWT_Code_Signing_Secret='-zsz8velXfhMeK6mexmgLTbDa8cXr79bO7CeN-I9M6L0WtpyjXKFTuZukY9VoKD-'
+export Auth0_Client_ID='eVbMiEiwFCmH8wsbsxb403xJYbrhJMwG'
