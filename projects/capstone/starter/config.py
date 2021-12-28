69559ea485447354c3d27b0a975f631b2365bd96
leonchan93 <38082954+leonchan93@users.noreply.github.com>
2021-12-28 22:54:26 +0000

    Update config.py

--- projects/capstone/starter/config.py
+++ projects/capstone/starter/config.py
@@ -11,4 +11,4 @@ DEBUG = True
 
 
 # TODO IMPLEMENT DATABASE URL
-SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:Projecta2008@localhost:5432/castingagency'
+SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:@localhost:5432/castingagency'
